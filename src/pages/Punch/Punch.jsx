d35ac2abf24cbefb844b39a039085f9e63822fb3
照錡 <p19901209@gmail.com>
2023-10-25 09:32:15 +0800

    Merge remote-tracking branch 'origin/Kama' into jeff 1025

--- src/pages/Punch/Punch.jsx
+++ src/pages/Punch/Punch.jsx
@@ -1,196 +1,155 @@
 import React, { useEffect, useState } from "react";
 import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import useLocalStorageValue from "../../hooks/useLocalStorageValue";
 import "leaflet/dist/leaflet.css";
 import { Button, LinearProgress, CircularProgress } from "@mui/material";
-import liff from "@line/liff";
 import { postData } from "../../utils/api";
-
-const LINE_ID = process.env.REACT_APP_LINEID;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
 
 const Map = () => {
-  const [selectedLocation, setSelectedLocation] = useState(null);
-  const [userProfile, setUserProfile] = useState(null);
-  const [punchState, setPunchState] = useState(1); //1為沒打卡，2打卡成功，3失敗
-  const [apiData, setApiData] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+	const [selectedLocation, setSelectedLocation] = useState(null);
+	// 1: 沒打卡，2: 打卡成功，3: 失敗
+	const [punchState, setPunchState] = useState(1);
+	const [punchTime, setPunchTime] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const userProfile = useLocalStorageValue("userProfile");
 
-  const initLine = () => {
-    liff.init(
-      { liffId: LINE_ID },
-      () => {
-        if (liff.isLoggedIn()) {
-          runApp();
-        } else {
-          liff.login();
-        }
-      },
-      (err) => console.error(err)
-    );
-  };
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				const { latitude, longitude } = position.coords;
+				setSelectedLocation({ lat: latitude, lng: longitude });
+				// setUserProfile(JSON.parse(localStorage.getItem("userProfile")));
+			});
+		}
+	}, [navigator.geolocation]);
 
-  useEffect(() => {
-    initLine();
-  }, []);
+	// 需要把 Marker 的 icon 弄出來，不然會顯示錯誤圖片
+	const customIcon = L.icon({
+		iconUrl: `${userProfile?.pictureUrl}.png`,
+		iconSize: [48, 48],
+		iconAnchor: [24, 24],
+		popupAnchor: [0, -28],
+		className: "rounded-full",
+	});
 
-  const runApp = () => {
-    const accessToken = liff.getAccessToken();
-    if (accessToken) {
-      //console.log(accessToken);
-      localStorage.setItem("accessToken", JSON.stringify(accessToken));
-    }
-    liff.getProfile().then((profile) => {
-      if (profile?.displayName) {
-        delete profile.statusMessage;
-        delete profile.userId;
-        setUserProfile(profile);
-      }
-    });
-  };
+	// 提交打卡按鈕
+	const handleSubmitPunch = async () => {
+		setIsLoading(true);
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        setSelectedLocation({ lat: latitude, lng: longitude });
-      });
-    }
-  }, [navigator.geolocation, userProfile]);
+		const fd = new FormData();
+		fd.append("latitude", selectedLocation?.lat.toFixed(6));
+		fd.append("longitude", selectedLocation?.lng.toFixed(6));
 
-  //需要把Marker的icon弄出來，不然會顯示錯誤圖片
-  const customIcon = L.icon({
-    iconUrl: `${userProfile?.pictureUrl}`,
-    iconSize: [48, 48],
-    iconAnchor: [24, 24],
-    popupAnchor: [0, -28],
-    className: "rounded-full",
-  });
+		postData("clockPunch", fd).then((result) => {
+			setIsLoading(false);
+			if (result.status) {
+				const data = result.result;
+				setPunchState(2);
 
-  // 提交打卡按鈕
-  const handleSubmitPunch = async () => {
-    setIsLoading(true);
+				const dateTime = data.result.occurred;
+				setPunchTime(dateTime.split("T"));
+			} else {
+				setPunchState(3);
+			}
+		});
+	};
 
-    const params = {
-      latitude: selectedLocation?.lat.toFixed(6),
-      longitude: selectedLocation?.lng.toFixed(6),
-    };
-    const queryParams = Object.keys(params)
-      .map((key) => `${key}=${encodeURIComponent(params[key])}`)
-      .join("&");
-
-    // clockPunch / `${queryParams}`;
-    postData(`clockPunch?${queryParams}`).then((response) => {
-      if (response.result.response === 200) {
-        setPunchState(2);
-        setApiData(response.result.result.occurred);
-        setIsLoading(false);
-      } else {
-        setPunchState(3);
-        console.log(response);
-        setIsLoading(false);
-      }
-    });
-  };
-
-  return (
-    <div
-      style={{
-        position: "fixed",
-        top: "55px",
-        left: 0,
-        zIndex: "1025",
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      {selectedLocation ? (
-        <MapContainer
-          center={selectedLocation}
-          zoom={13}
-          // scrollWheelZoom={false}
-          dragging={false}
-          style={{ height: "100%", width: "100%" }}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={selectedLocation} icon={customIcon}>
-            <Popup closeButton={false} open={true}>
-              {punchState === 1 ? (
-                <Button
-                  variant="contained"
-                  //sx={{ width: "120px", margin: "-5px" }}
-                  className="w-32 h-10"
-                  disabled={isLoading}
-                  onClick={() => {
-                    handleSubmitPunch();
-                  }}
-                >
-                  {isLoading ? (
-                    <CircularProgress color="inherit" size={20} />
-                  ) : (
-                    "打卡"
-                  )}
-                </Button>
-              ) : (
-                <p variant="contained" className="w-32 ">
-                  {punchState === 2 ? apiData : "出現錯誤"}
-                </p>
-              )}
-            </Popup>
-          </Marker>
-        </MapContainer>
-      ) : (
-        <LinearProgress />
-      )}
-    </div>
-  );
+	return (
+		<div
+			style={{
+				position: "fixed",
+				top: "55px",
+				left: 0,
+				zIndex: "1025",
+				height: "100%",
+				width: "100%",
+			}}>
+			{selectedLocation ? (
+				<MapContainer
+					center={selectedLocation}
+					zoom={13}
+					// scrollWheelZoom={false}
+					dragging={false}
+					style={{ height: "100%", width: "100%" }}>
+					<TileLayer
+						attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+					<Marker position={selectedLocation} icon={customIcon}>
+						<Popup closeButton={false} open={true} className="w-64 h-48">
+							{punchState === 1 ? (
+								isLoading ? (
+									<CircularProgress color="inherit" size={60} />
+								) : (
+									<Button
+										variant="contained"
+										className="!text-3xl tracking-widest flex-col gap-2 !p-3"
+										onClick={(event) => {
+											event.stopPropagation();
+											handleSubmitPunch();
+										}}
+										fullWidth>
+										<FontAwesomeIcon icon={faCalendarCheck} style={{ fontSize: "5.5rem" }} />
+										<p className="text-3xl !m-0">立即打卡</p>
+									</Button>
+								)
+							) : punchState === 2 ? (
+								<>
+									<p className="text-4xl !mt-0 !mb-2">🎉</p>
+									<p className="text-2xl font-bold tracking-wide !mt-0 !mb-1">打卡成功啦</p>
+									{punchTime.map((time, index) => (
+										<React.Fragment key={index}>
+											<p className="text-lg font-bold tracking-wide leading-tight !my-0">{time}</p>
+										</React.Fragment>
+									))}
+								</>
+							) : (
+								<p variant="contained">出現錯誤</p>
+							)}
+						</Popup>
+					</Marker>
+				</MapContainer>
+			) : (
+				<LinearProgress />
+			)}
+		</div>
+	);
 };
 export default Map;
 
 //生成地圖右下的定位座標按鈕
 const UserPosition = ({ title, markerPosition, description }) => {
-  const map = useMap();
-  const helpDivRef = React.useRef(null);
+	const map = useMap();
+	const helpDivRef = React.useRef(null);
 
-  useEffect(() => {
-    const createButtonControl = () => {
-      const MapHelp = L.Control.extend({
-        onAdd: () => {
-          const helpDiv = L.DomUtil.create("div", "custom-button");
-          helpDivRef.current = helpDiv;
+	useEffect(() => {
+		const createButtonControl = () => {
+			const MapHelp = L.Control.extend({
+				onAdd: () => {
+					const helpDiv = L.DomUtil.create("div", "custom-button");
+					helpDivRef.current = helpDiv;
 
-          const iconDiv = document.createElement("div");
-          iconDiv.className = "locationicon-container";
-          helpDiv.appendChild(iconDiv);
+					const iconDiv = document.createElement("div");
+					iconDiv.className = "locationicon-container";
+					helpDiv.appendChild(iconDiv);
 
-          helpDiv.addEventListener("click", () => {
-            map.setView(markerPosition, map.getZoom());
-          });
-          return helpDiv;
-        },
-      });
-      return new MapHelp({ position: "bottomright" });
-    };
+					helpDiv.addEventListener("click", () => {
+						map.setView(markerPosition, map.getZoom());
+					});
+					return helpDiv;
+				},
+			});
+			return new MapHelp({ position: "bottomright" });
+		};
 
-    const control = createButtonControl();
-    control.addTo(map);
-    return () => {
-      helpDivRef.current.remove();
-    };
-  }, [map, markerPosition, description]);
-  return null;
+		const control = createButtonControl();
+		control.addTo(map);
+		return () => {
+			helpDivRef.current.remove();
+		};
+	}, [map, markerPosition, description]);
+	return null;
 };
-
-// import React from "react";
-
-// const Punch = () => {
-// 	return (
-// 		<>
-// 			<h3 className="text-3xl font-bold underline">Punch</h3>
-// 		</>
-// 	);
-// };
-
-// export default Punch;
