19dba4d031bc9e1aeb6c93927f217f43175d5ee4
K2309N0184459\Kama <yuanrong234001@gmail.com>
2023-11-30 16:12:40 +0800

    [updated] RWD list Btn擋住問題

--- src/components/RWDTable/RWDTable.jsx
+++ src/components/RWDTable/RWDTable.jsx
@@ -37,7 +37,7 @@ const RWDTable = ({
 
 	if (isSmallScreen) {
 		return (
-			<div className="flex flex-col gap-3 pt-1 pb-3.5">
+			<div className="flex flex-col gap-3 pt-1 pb-[40%]">
 				{!isLoading ? (
 					data && data.length > 0 ? (
 						data.map((item, rowIndex) => (
