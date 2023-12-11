c957fc96d71b7c0067b3093e83e9352aeae9ecb0
Kama <yuanrong234001@gmail.com>
2023-12-11 16:27:22 +0800

    [updated] 瀏覽器標題名稱變動

--- src/components/Breadcrumbs/CustomBreadcrumbs.jsx
+++ src/components/Breadcrumbs/CustomBreadcrumbs.jsx
@@ -1,61 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import { NavLink, useLocation } from "react-router-dom";
 
-const CustomBreadcrumbs = () => {
+const nameMappings = [
+	{ name: "userinfo", display: "帳戶資訊" },
+	{ name: "sites", display: "案場" },
+	{ name: "constructionTypes", display: "工程類別" },
+	{ name: "constructionsummary", display: "施工清單" },
+	{ name: "users", display: "人事管理" },
+	{ name: "dispatchList", display: "派工清單" },
+	{ name: "project", display: "專案管理" },
+	{ name: "setting", display: "設定" },
+	{ name: "punch", display: "打卡" },
+	{ name: "maps", display: "地圖" },
+	{ name: "dispatchcalendar", display: "派工行事曆" },
+	{ name: "attendance_calendar", display: "打卡與考勤" },
+];
+
+const CustomBreadcrumbs = React.memo(() => {
 	const location = useLocation();
 	const pathnames = location.pathname.split("/").filter((x) => x);
+	const [displayText, setDisplayText] = useState(null);
 
-	const nameMappings = [
-		{ name: "userinfo", display: "帳戶資訊" },
-		{ name: "sites", display: "案場" },
-		{ name: "constructionTypes", display: "工程類別" },
-		{ name: "constructionsummary", display: "施工清單" },
-		{ name: "users", display: "人事管理" },
-		{ name: "dispatchList", display: "派工清單" },
-		{ name: "project", display: "專案管理" },
-		{ name: "setting", display: "設定" },
-		{ name: "punch", display: "打卡" },
-		{ name: "maps", display: "地圖" },
-		{ name: "dispatchcalendar", display: "派工行事曆" },
-		{ name: "attendance_calendar", display: "打卡與考勤" },
-	];
+	useEffect(() => {
+		let fullName = [];
+		pathnames.map((name, index) => {
+			const isLast = index === pathnames.length - 1;
+			let decodedName = decodeURIComponent(name);
+			if (decodedName.includes("+")) {
+				decodedName = decodedName.split("+")[0];
+			}
+
+			const mappedItem = nameMappings.find((item) => item.name === decodedName);
+			const displayText = mappedItem ? mappedItem.display : decodedName;
+			fullName.push(displayText);
+
+			if (isLast) setDisplayText(fullName);
+		});
+
+		const title = fullName.length > 0 ? fullName.join(" - ") + " | YuanRong Tech" : "YuanRong Tech";
+		document.title = title;
+	}, [location]);
 
 	return (
 		<Breadcrumbs
 			aria-label="breadcrumb"
 			maxItems={3}
 			className={`pt-2 sm:pt-3 pb-2 sm:pb-0 px-5 !text-sm ${pathnames.length > 0 ? "" : "!pt-0 !pb-0"}`}>
-			{pathnames.length > 0 && (
+			{pathnames.length > 0 ? (
 				<Link component={NavLink} color="inherit" underline="hover" to="/">
 					首頁
 				</Link>
+			) : (
+				(() => {
+					document.title = "YuanRong Tech";
+					return null;
+				})()
 			)}
 			{pathnames.map((name, index) => {
 				const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
 				const isLast = index === pathnames.length - 1;
-				let decodedName = decodeURIComponent(name);
-				if (decodedName.includes("+")) {
-					decodedName = decodedName.split("+")[0];
-				}
-
-				const mappedItem = nameMappings.find((item) => item.name === decodedName);
-				const displayText = mappedItem ? mappedItem.display : decodedName;
 
 				return isLast ? (
 					<Typography key={name} color="textPrimary" className="!text-sm">
-						{displayText}
+						{!!displayText && displayText[index]}
 					</Typography>
 				) : (
 					<Link key={name} component={NavLink} to={routeTo} color="inherit" underline="hover">
-						{displayText}
+						{!!displayText && displayText[index]}
 					</Link>
 				);
 			})}
 		</Breadcrumbs>
 	);
-};
+});
 
 export default CustomBreadcrumbs;
