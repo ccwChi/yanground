37a920ac2dd5227628cdfa0b58428df4a32b75af
K2309N0184459\Kama <yuanrong234001@gmail.com>
2024-01-30 09:58:22 +0800

    [Modified] 考勤報表部門改為多選/輸出優化

--- src/pages/AttendanceReport/AttendanceReport.jsx
+++ src/pages/AttendanceReport/AttendanceReport.jsx
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from "react";
 import { useForm, FormProvider, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { format } from "date-fns";
 // MUI
 import Autocomplete from "@mui/material/Autocomplete";
 import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import Checkbox from "@mui/material/Checkbox";
 import TextField from "@mui/material/TextField";
-import useMediaQuery from "@mui/material/useMediaQuery";
+// import useMediaQuery from "@mui/material/useMediaQuery";
 import CircularProgress from "@mui/material/CircularProgress";
 import Divider from "@mui/material/Divider";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 // Components
 import PageTitle from "../../components/Guideline/PageTitle";
 import InputTitle from "../../components/Guideline/InputTitle";
@@ -16,24 +21,29 @@ import ControlledDatePicker from "../../components/DatePicker/ControlledDatePick
 // Utils
 import { getData } from "../../utils/api";
 
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
 const SalaryCalculation = () => {
 	// 部門清單
 	const [departmentList, setDepartmentList] = useState([]);
-	const isTargetScreen = useMediaQuery("(max-width:767.98px)");
+	// const isTargetScreen = useMediaQuery("(max-width:767.98px)");
+	// const fixedOptions = { label: "全部", id: "" };
 
 	// 取得部門資料
 	useEffect(() => {
 		getData("department").then((result) => {
 			const data = result.result.content;
 			const formattedDep = data.map((dep) => ({ label: dep.name, id: dep.id }));
-			const optionsWithAll = [{ label: "全部", id: "" }, ...formattedDep];
-			setDepartmentList(optionsWithAll);
+			// const optionsWithAll = [fixedOptions, ...formattedDep];
+			// setDepartmentList(optionsWithAll);
+			setDepartmentList(formattedDep);
 		});
 	}, []);
 
 	// 初始預設 default 值
 	const defaultValues = {
-		department: null,
+		department: [],
 		date: new Date(),
 	};
 
@@ -50,21 +60,22 @@ const SalaryCalculation = () => {
 		defaultValues,
 		resolver: yupResolver(schema),
 	});
-	const {
-		control,
-		handleSubmit,
-		formState: { errors, isDirty },
-	} = methods;
+	const { control, handleSubmit } = methods;
 
 	// 提交表單資料到後端並執行相關操作
 	const onSubmit = (data) => {
 		const fd = new FormData();
 
-		for (let key in data) {
-			fd.append(key, data[key]);
-		}
+		// 使用 map 函數將每個對象的 id 提取出來, 並將空字符串過濾掉
+		const idList = data["department"].map((item) => item.id);
+		const filteredIdList = idList.filter((id) => id !== "");
 
-		console.log(data);
+		fd.append("department", filteredIdList);
+		fd.append("date", format(data["date"], "yyyy-MM"));
+
+		for (var pair of fd.entries()) {
+			console.log(pair[0] + ", " + pair[1]);
+		}
 		// sendDataToBackend(fd, "temporaryannouncement", _date);
 	};
 
@@ -78,9 +89,9 @@ const SalaryCalculation = () => {
 						<div className="flex flex-col">
 							<p className="mb-4">選擇您要查看的年份和月份，然後點擊『生成報表』按鈕，以載出當月考勤結算的報表。</p>
 							<Divider className="!border-[1px] !mb-6" />
-							<div className="flex md:flex-row flex-col md:items-end gap-3 mb-2">
+							<div className="flex flex-col gap-3 mb-2">
 								{/* 部門 */}
-								<div className="inline-flex flex-col">
+								<div className="inline-flex flex-col w-full">
 									<InputTitle classnames="whitespace-nowrap" title={"部門"} required={false} />
 									<Controller
 										control={control}
@@ -89,19 +100,42 @@ const SalaryCalculation = () => {
 											const { onChange, value } = field;
 											return (
 												<Autocomplete
+													multiple
+													// limitTags={2}
+													disableCloseOnSelect
 													options={departmentList}
-													className="md:w-[200px] w-full"
 													value={value}
 													onChange={(event, selectedOptions) => {
 														onChange(selectedOptions);
+														// onChange([...[fixedOptions], ...selectedOptions.filter((option) => option.id !== "")]);
 													}}
 													isOptionEqualToValue={(option, value) => option.id === value.id}
+													renderTags={(tagValue, getTagProps) =>
+														tagValue.map((option, index) => (
+															<Chip
+																label={option.label}
+																{...getTagProps({ index })}
+																// disabled={option.id === ""}
+															/>
+														))
+													}
+													renderOption={(props, option, { selected }) => (
+														<li {...props}>
+															<Checkbox
+																icon={icon}
+																checkedIcon={checkedIcon}
+																style={{ marginRight: 8 }}
+																checked={selected}
+															/>
+															{option.label}
+														</li>
+													)}
 													renderInput={(params) => (
 														<TextField
 															{...params}
 															className="inputPadding bg-white"
 															placeholder="請選擇部門"
-															sx={{ "& > div": { padding: "0 !important" } }}
+															// sx={{ "& > div": { padding: "0 !important" } }}
 															InputProps={{
 																...params.InputProps,
 																endAdornment: (
@@ -117,6 +151,7 @@ const SalaryCalculation = () => {
 													)}
 													loading={departmentList.length <= 0}
 													loadingText={"載入中..."}
+													fullWidth
 												/>
 											);
 										}}
@@ -131,16 +166,18 @@ const SalaryCalculation = () => {
 										views={["month", "year"]}
 										format={"yyyy 年 MM 月"}
 										minDate={new Date("2023-11")}
-										sx={{ width: isTargetScreen ? "100%" : "max-content" }}
+										// sx={{ width: isTargetScreen ? "100%" : "max-content" }}
 									/>
 								</div>
-								<Button
-									type="submit"
-									variant="contained"
-									color="dark"
-									className="!text-base !h-12 whitespace-nowrap md:w-max w-full">
-									生成報表
-								</Button>
+								<div className="inline-flex">
+									<Button
+										type="submit"
+										variant="contained"
+										color="dark"
+										className="!text-base !h-12 whitespace-nowrap md:w-max w-full">
+										生成報表
+									</Button>
+								</div>
 							</div>
 							<div className="flex mt-2">
 								<p className="!my-0 text-rose-400 font-bold text-xs !me-1">＊</p>
